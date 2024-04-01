585b922d516fe25e2b746f21c697fb083317dfc5
ahmedpasic19 <ahmedpasic15@gmail.com>
2024-04-01 21:51:27 +0200

    Header on each page
    Changes:
    - Added header to each page

--- src/app/(app)/(records)/expense/page.tsx
+++ src/app/(app)/(records)/expense/page.tsx
@@ -4,11 +4,14 @@ import { getUserRecordsByType } from '../actions'
 import AddRecordDialog from '../components/dialogs/add-record-dialog'
 import RecordsTable from '../components/records-table'
 
+import { H4 } from '@/components/ui/typography'
+
 export default async function ExpensePage() {
    const data = await getUserRecordsByType('EXPENSE')
 
    return (
       <div className="page">
+         <H4>Expense</H4>
          <div className="mb-2 w-full flex items-start">
             <Suspense>
                <AddRecordDialog type="EXPENSE" />
--- src/app/(app)/(records)/income/page.tsx
+++ src/app/(app)/(records)/income/page.tsx
@@ -4,11 +4,14 @@ import { getUserRecordsByType } from '../actions'
 import AddRecordDialog from '../components/dialogs/add-record-dialog'
 import RecordsTable from '../components/records-table'
 
+import { H4 } from '@/components/ui/typography'
+
 export default async function IncomePage() {
    const data = await getUserRecordsByType('INCOME')
 
    return (
       <div className="page">
+         <H4>Income</H4>
          <div className="mb-2 w-full flex items-start">
             <Suspense>
                <AddRecordDialog type="INCOME" />
