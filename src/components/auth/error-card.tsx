ea45bfd5c6ed64bf2f363a59afaf3b757a2a1c88
ahmedpasic19 <ahmedpasic15@gmail.com>
2024-04-04 16:06:05 +0200

    Fixed dumb "reduce" issue
    Changes:;
    - Fixed DB config
    - Added an error-card

--- src/components/auth/error-card.tsx
+++ src/components/auth/error-card.tsx
@@ -1,5 +1,17 @@
-// import { ExclamationTriangleIcon } from "@radix-ui/react-icons";
+import { ExclamationTriangleIcon } from '@radix-ui/react-icons'
+
+import { CardWrapper } from '@/components/auth/card-wrapper'
 
 export const ErrorCard = () => {
-   return <div>Error card</div>
+   return (
+      <CardWrapper
+         headerLabel="Oops! Something went wrong!"
+         backButtonHref="/auth/login"
+         backButtonLabel="Back to login"
+      >
+         <div className="w-full flex justify-center items-center">
+            <ExclamationTriangleIcon className="text-destructive" />
+         </div>
+      </CardWrapper>
+   )
 }
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,20 +1,18 @@
 import { PrismaClient } from '@prisma/client'
 
-import { env } from '../env/server.mjs'
-
 declare global {
    // eslint-disable-next-line no-var
    var prisma: PrismaClient | undefined
 }
 
 const prisma =
-   global.prisma ||
+   globalThis.prisma ||
    new PrismaClient({
-      log: env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
+      log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
    })
 
-if (env.NODE_ENV !== 'production') {
-   global.prisma = prisma
+if (process.env.NODE_ENV !== 'production') {
+   globalThis.prisma = prisma
 }
 
 export default prisma
