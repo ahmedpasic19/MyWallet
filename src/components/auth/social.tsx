e52cf8e173ae92f66fe44b8822fc5859ee1296fd
ahmedpasic19 <ahmedpasic15@gmail.com>
2025-04-06 11:38:08 +0200

    Removed Github auth

--- src/components/auth/social.tsx
+++ src/components/auth/social.tsx
@@ -12,7 +12,7 @@ export const Social = () => {
    const searchParams = useSearchParams()
    const callbackUrl = searchParams.get('callbackUrl')
 
-   const onClick = (provider: 'google' | 'github') => {
+   const onClick = (provider: 'google') => {
       signIn(provider, {
          callbackUrl: callbackUrl || DEFAULT_LOGIN_REDIRECT,
       })
@@ -22,9 +22,6 @@ export const Social = () => {
       <div className="flex items-center w-full gap-x-2">
          <Button size="lg" className="w-full" variant="outline" onClick={() => onClick('google')}>
             <FcGoogle className="h-5 w-5" />
-         </Button>
-         <Button size="lg" className="w-full" variant="outline" onClick={() => onClick('github')}>
-            <FaGithub className="h-5 w-5" />
          </Button>
       </div>
    )
