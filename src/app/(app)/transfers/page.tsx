7c960aa37f96fd2c4273098e56bb665a05fa76f4
ahmedpasic19 <ahmedpasic15@gmail.com>
2025-04-09 00:24:00 +0200

    Transfered SSR
    Changes:
     - Added loading page
     - Handled client components with next/dynamic

--- src/app/(app)/transfers/page.tsx
+++ src/app/(app)/transfers/page.tsx
@@ -1,11 +1,21 @@
-import React, { Suspense } from 'react'
+import React from 'react'
+
+import dynamic from 'next/dynamic'
 
 import DashboardTableSkeleton from '../dashboard/components/skeleton/dashboard-table-skeleton'
 
 import { getUserTransfers } from './actions'
-import AddTransferDialog from './components/dialogs/add-transfer-dialog'
-import TransfersTable from './components/transfers-table'
+import ButtonSkeleton from '@/components/skeleton/button-skeleton'
 import { H4 } from '@/components/ui/typography'
+
+const AddTransferDialog = dynamic(() => import('./components/dialogs/add-transfer-dialog'), {
+   ssr: false,
+   loading: () => <ButtonSkeleton />,
+})
+const TransfersTable = dynamic(() => import('./components/transfers-table'), {
+   ssr: false,
+   loading: () => <DashboardTableSkeleton noBtn />,
+})
 
 export default async function TransfersPage() {
    const data = await getUserTransfers()
@@ -14,14 +24,10 @@ export default async function TransfersPage() {
       <div className="page">
          <H4>Transfers</H4>
          <div className="mb-2 w-full flex items-start">
-            <Suspense>
-               <AddTransferDialog />
-            </Suspense>
+            <AddTransferDialog />
          </div>
          <div className="w-full ">
-            <Suspense fallback={<DashboardTableSkeleton noBtn />}>
-               <TransfersTable data={data?.transfers} />
-            </Suspense>
+            <TransfersTable data={data?.transfers} />
          </div>
       </div>
    )
